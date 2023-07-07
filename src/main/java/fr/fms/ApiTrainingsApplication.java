c1078ad24a277a29fe9959fb04da7d0c0b64dd0c
Ludovic Dupre <dupreludovic1@gmail.com>
2023-07-07 15:13:45 +0200

    Début sécu

--- src/main/java/fr/fms/ApiTrainingsApplication.java
+++ src/main/java/fr/fms/ApiTrainingsApplication.java
@@ -22,11 +22,11 @@ public class ApiTrainingsApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-	trainingRepository.save(new Training(null, "Java","Java Standard Edition 8 sur 5 jours", 3500.0, "../src/main/resources/static/java.jpg", 1));
-	trainingRepository.save(new Training(null, "DotNet","DotNet & entity framework en 5 jours", 2750.0, "../src/main/resources/static/asp.webp", 1));
-	trainingRepository.save(new Training(null, "PowerBi","Business Intelligence en 5 jours", 3000.0, "../src/main/resources/static/powerbi.png", 1));
-	trainingRepository.save(new Training(null, "NodeJs","Prise en main de NodeJs/Express 2 jours", 3500.0, "../src/main/resources/static/nodejs.png", 1));
-	trainingRepository.save(new Training(null, "Php","Initiation au Dev/Web avec hp 4 jours", 1300.0, "../src/main/resources/static/php.png", 1));
+	trainingRepository.save(new Training(null, "Java","Java Standard Edition 8 sur 5 jours", 3500.0, "\\assets\\java.jpg", 1));
+	trainingRepository.save(new Training(null, "DotNet","DotNet & entity framework en 5 jours", 2750.0, "\\assets\\asp.webp", 1));
+	trainingRepository.save(new Training(null, "PowerBi","Business Intelligence en 5 jours", 3000.0, "\\assets\\powerbi.png", 1));
+	trainingRepository.save(new Training(null, "NodeJs","Prise en main de NodeJs/Express 2 jours", 3500.0, "\\assets\\nodejs.png", 1));
+	trainingRepository.save(new Training(null, "Php","Initiation au Dev/Web avec hp 4 jours", 1300.0, "\\assets\\php.png", 1));
 
 	System.out.println("Ready!");
 
--- src/main/java/fr/fms/web/TrainingController.java
+++ src/main/java/fr/fms/web/TrainingController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 @CrossOrigin("*")
 @RestController
+//@RequestMapping("/api")
 public class TrainingController {
 
     @Autowired
