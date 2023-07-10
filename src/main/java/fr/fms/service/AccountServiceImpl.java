ed19770f64bd164631a479ab53043f4c409b4e18
Ludovic Dupre <dupreludovic1@gmail.com>
2023-07-10 09:03:37 +0200

    flop

--- src/main/java/fr/fms/service/AccountServiceImpl.java
+++ src/main/java/fr/fms/service/AccountServiceImpl.java
@@ -6,8 +6,8 @@ import fr.fms.entities.AppRole;
 import fr.fms.entities.AppUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
